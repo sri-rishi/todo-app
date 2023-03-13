bdced6d4fd4ea97b4e2a5042c216cb4ad3c8f483
sri-rishi <sririshi2612@gmail.com>
2023-03-13 13:01:09 +0530

    feat: added delete functionaliy and refactored

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inputBox = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
 const addTodoBtn = document.getElementById("add-todo-btn");
 
+
 let newTodo; 
 
 let todoArray = [
@@ -15,39 +16,55 @@ let todoArray = [
     }
 ];
 
-window.onload= () => {
-    todoArray.forEach((item) => {
-        const todoItemCard = document.createElement("li");
-        const crossBtn = document.createElement("button")
-        crossBtn.innerText = "Delete"
-    
-        todoItemCard.innerHTML = item.name;
-        todoItemCard.appendChild(crossBtn)
-    
-        todoList.appendChild(todoItemCard);
-    })
-};
+const deleteTodo = (id) =>  {
+    todoArray = [...todoArray].filter(todo => todo.id !== id)
+    console.log(todoArray)
+    const todoItemList = document.querySelectorAll(".todo-item");
+    todoItemList.forEach((item) => {
+        if(parseInt(item.dataset.id) === id ) {
+            item.style.display = parseInt(item.dataset.id) === id && "none" 
+        }
+    }
+    )
+}
+
+const createNewTodoItem = (todo) => {
+    const todoItemCard = document.createElement("LI");
+    todoItemCard.classList.add("todo-item")
+    todoItemCard.dataset.id = todo.id
+    const crossBtn = document.createElement("BUTTON");
+    crossBtn.classList.add("delete-todo-btn");
+    crossBtn.innerText = "Delete"
+    crossBtn.onclick = () => {
+        deleteTodo(todo.id)
+    }
+    todoItemCard.innerHTML = todo.name;
+    todoItemCard.appendChild(crossBtn)
+
+    return todoItemCard
+}
+
+todoArray.forEach((item) => {
+    const todoItem =  createNewTodoItem(item)
+
+    todoList.appendChild(todoItem);
+})
+
+
 
 inputBox.addEventListener("input", (e) => {
-    console.log(e.target.value)
     newTodo = e.target.value
 });
 
 const renderTodo = (todo) => {
-    const todoItemCard = document.createElement("li");
-    
-    const crossBtn = document.createElement("button")
-    crossBtn.innerText = "Delete"
-    todoItemCard.innerHTML = todo.name;
-
-    todoItemCard.appendChild(crossBtn)
-    
-    todoList.appendChild(todoItemCard);
+    const todoItem = createNewTodoItem(todo)
+    todoList.appendChild(todoItem);
 };
+
  
 addTodoBtn.addEventListener("click", () => {
-    console.log(newTodo);
     let updatedItem = {id: 4, name: newTodo}
     todoArray.push(updatedItem);
     renderTodo(updatedItem)
 });
+
